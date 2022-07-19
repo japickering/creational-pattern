576876f417a09463247ee7b10aeff2db39e9a703
James Pickering <james.pickering@gmail.com>
2022-07-19 14:19:45 +0100

    tsconfig edits and comments

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,53 @@
-import Bike from './classes/Bike';
+const enum BikePrices {
+  UsualPrice = 399,
+  TownBikePrice = 299,
+}
 
-const townBike = new Bike('town bike', 12, 299, false);
+interface BikeModel {
+  name: string;
+  gears: number;
+  price: number;
+}
+
+// Creation design pattern
+// define our base class
+export default class Bike implements BikeModel {
+  public name: string;
+  public gears: number;
+  public price: number;
+
+  public usualPrice = BikePrices.UsualPrice;
+  public townBikePrice = BikePrices.UsualPrice;
+  private taxRate = 0.2; // 20% VAT
+
+  constructor(name: string, gears: number, price: number) {
+    this.name = name;
+    this.gears = gears;
+    this.price = price;
+  }
+
+  public getName() {
+    return this.name;
+  }
+
+  public getGears() {
+    return this.gears;
+  }
+
+  private calcPrice(val: number) {
+    return Math.floor(val + val * this.taxRate);
+  }
+
+  public getPrice() {
+    // to compare strings ensure bike name has matching case
+    const name = this.name.toLowerCase();
+
+    return name === 'town bike' ? this.calcPrice(this.townBikePrice) : this.calcPrice(this.usualPrice);
+  }
+}
+
+const townBike = new Bike('town bike', 12, 299);
 
 console.log(townBike.getName());
 console.log('has gears ', townBike.getGears());
-console.log('lock fitted ', townBike.isLockFitted());
 console.log('price ', townBike.getPrice());
